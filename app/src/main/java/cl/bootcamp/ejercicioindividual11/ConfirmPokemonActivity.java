6218068e2a781a090df6187a87ccff6b3fa1421c
KathyAlde21 <katherinealdereteg@gmail.com>
2024-12-04 17:52:48 -0300

    first commit

---
+++ app/src/main/java/cl/bootcamp/ejercicioindividual11/ConfirmPokemonActivity.java
@@ -0,0 +1,30 @@
+package cl.bootcamp.ejercicioindividual11;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ConfirmPokemonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_pokemon);
+
+        // Recuperar el nombre del Pokémon seleccionado
+        TextView pokemonNameText = findViewById(R.id.pokemon_name_text);
+        String pokemonName = getIntent().getStringExtra("pokemon_name");
+
+        // Mostrar el nombre del Pokémon
+        if (pokemonName != null) {
+            pokemonNameText.setText("Has seleccionado a: " + pokemonName);
+        } else {
+            pokemonNameText.setText("No se seleccionó ningún Pokémon.");
+
+        }
+    }
+}
